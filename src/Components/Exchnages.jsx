fc026a6cd2bdde320f42da8d8088e8265f8981cf
chesthajain <cheshtajain1712@gmail.com>
2024-09-11 15:12:23 +0530

    Loader

--- src/Components/Exchnages.jsx
+++ src/Components/Exchnages.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import { server } from "../index";
+import {
+  Container,
+  Heading,
+  HStack,
+  Image,
+  Text,
+  VStack
+} from "@chakra-ui/react";
+import Loader from "./Loader";
+
 
 const Exchnages = () => {
+  
+  conts [loading,setLoading] = useState(true);
+
+
+
   return (
-    <div>Exchnages</div>
+    <Container maxW={"container.xl"}>
+      {loading ? (
+        <Loader />
+      ):(
+        <>
+
+        </>
+      )}
+    </Container>
   )
 }
 

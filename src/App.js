0fa1d9cf0de682f21e3eb3b91f499f2963859db8
chesthajain <cheshtajain1712@gmail.com>
2024-09-11 14:44:18 +0530

    basic structure of app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Coins from "./Components/Coins";
+import Exchnages from "./Components/Exchnages";
+import CoinDetails from "./Components/CoinDetails.jsx";
 
 function App() {
   return (
-   <div>
-    </div>
+
+   <Router>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/coins" element={<Coins />} />
+      <Route path="/exchnages" element={<Exchnages />} />
+      <Route path="/coin/:id" element={<CoinDetails />} />
+    </Routes>
+    <Footer />
+   </Router>
   );
 }
 

00a8d15dfa2c3e5eaea1a341af3bf1a8178367de
chesthajain <cheshtajain1712@gmail.com>
2024-09-11 20:54:44 +0530

    exchangescard

--- src/Components/Exchnages.jsx
+++ src/Components/Exchnages.jsx
@@ -10,11 +10,12 @@ import {
   VStack
 } from "@chakra-ui/react";
 import Loader from "./Loader";
+import ErrorComponent from "./ErrorComponent"
 
 
 const Exchnages = () => {
-  
-  conts [loading,setLoading] = useState(true);
+
+  const [loading,setLoading] = useState(true);
 
 
 
@@ -24,11 +25,51 @@ const Exchnages = () => {
         <Loader />
       ):(
         <>
-
+        <HStack wrap={"wrap"} justifyContent={"space-evenly"}>
+          {Exchnages.map((i) => (
+            <ExchangeCard
+            key={i.id}
+            name={i.id}
+            img={i.image}
+            rank={i.trust_score_rank}
+            url={i.url}
+            />
+          ))}
+        </HStack>
         </>
       )}
     </Container>
-  )
-}
+  );
+};
+
+const ExchangeCard = ({ name, img, rank, url}) => (
+  <a href={url} target={"blank"}>
+    <VStack
+    w={"52"}
+    shadow={"lg"}
+    p={"8"}
+    borderRadius={"lg"}
+    transition={"all 0.3s"}
+    m={"4"}
+    css={{
+      "&:hover": {
+        transform: "scale(1.1)",
+      },
+    }}
+    >
+      <Image 
+      src={img}
+      w={"10"}
+      h={"10"}
+      objectFit={"contain"}
+      alt={"Exchange"}
+      />
+      <Heading size={"md"} noOfLines={1}>
+        {rank}
+      </Heading>
+      <Text noOfLines={1}>{name}</Text>
+    </VStack>
+  </a>
+);
 
 export default Exchnages

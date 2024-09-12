b4c3688f71168b9f872501d8a072b21f852912ec
chesthajain <cheshtajain1712@gmail.com>
2024-09-12 15:25:17 +0530

    Home component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom";
 const Header = () => {
   return (
     <HStack p={"4"} shadow={"base"} bgColor={"blackAlpha.900"}>
-      <Button variant={"unstyles"} color={"white"}>
+      <Button variant={"unstyled"} color={"white"}>
         <Link to="/">Home</Link>
       </Button>
       <Button variant={"unstyled"} color={"white"}>
